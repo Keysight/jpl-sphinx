674372213e807b0330a67038a9969e917c4be235
Rachel Lou <rachel.lou@microsoft.com>
2021-07-15 17:53:08 -0700

    remove fib

--- resources/scripts/show_host_info.py
+++ resources/scripts/show_host_info.py
@@ -48,23 +48,6 @@ def get_argument_parser():
 
     return argument_parser
 
-def fibonacci(n):
-    """ Function for nth fibonacci, space optimized"""
-    a = 0
-    b = 1
-    if n < 0:
-        print("Incorrect input")
-    elif n == 0:
-        return 0
-    elif n == 1:
-        return b
-    else:
-        for i in range(1, n):
-            c = a + b
-            a = b
-            b = c
-        return b
-
 def get_parsed_arguments(test_args=None):
     """Get the parsed arguments"""
 
